675b9e7b1022c9a38d878ad2004b13b3451de03b
AniaB84 <145894449+AniaB84@users.noreply.github.com>
2024-04-04 10:40:29 +0200

    include/

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
 
+class Config:
+    # Klucz sekretny dla zabezpieczenia sesji i innych
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'trudno-mi-zgadnąć-coś-bardzo-tajnego'
+
+    # Konfiguracja bazy danych - tu zakładam użycie bazy SQLite
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
