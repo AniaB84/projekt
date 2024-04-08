46f60040deea46f5361030ab07596a5a9bf6e04b
AniaB84 <145894449+AniaB84@users.noreply.github.com>
2024-04-08 13:22:42 +0200

    share/

--- blog/routes.py
+++ blog/routes.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 from blog import app
+from blog.models import Entry
+app = Flask(__name__)  
+from faker import Faker
+from blog.models import Entry, db
 
-app = Flask(__name__, template_folder='templates')  
 
 @app.route("/")
 def index():
- return render_template("base.html")
+ all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+ return render_template("homepage.html", all_posts=all_posts)
 
+def generate_entries(how_many=10):
+   fake = Faker()
+   for i in range(how_many):
+       post = Entry(
+           title=fake.sentence(),
+           body='\n'.join(fake.paragraphs(15)),
+           is_published=True
+       )
+       db.session.add(post)
+   db.session.commit()
+   
 if __name__ == "__main__":
     app.run(debug=True)
+   
--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 
-class Config:
-    # Klucz sekretny dla zabezpieczenia sesji i innych
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'trudno-mi-zgadnąć-coś-bardzo-tajnego'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
 
-    # Konfiguracja bazy danych - tu zakładam użycie bazy SQLite
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+ SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key" 
+ SQLALCHEMY_DATABASE_URI = ( os.environ.get('DATABASE_URL') or'sqlite:///' + os.path.join(BASE_DIR, 'blog.db') )
+ SQLALCHEMY_TRACK_MODIFICATIONS = False
