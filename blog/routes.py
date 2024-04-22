1cc078b25a6fee5cd0ec1b378439d24e7533e343
AniaB84 <145894449+AniaB84@users.noreply.github.com>
2024-04-22 11:14:53 +0200

    run.py

--- blog/routes.py
+++ blog/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template
-from blog import app
+from blog import app, db
 from blog.models import Entry
 
-
-@app.route("/", endpoint='index')
+@app.route("/", endpoint="index")
 def index():
-    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc()).all()
-    return render_template("homepage.html", all_posts=all_posts)
+ return render_template("base.html") 
 
-   
+@app.route("/home", endpoint="home")
+def home():
+ all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc()).all()
+ return render_template("homepage.html", all_posts=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
-   
---
+++ run.py
@@ -0,0 +1,5 @@
+from blog import app
+from blog import routes
+
+if __name__ == "__main__":
+    app.run(debug=True)
