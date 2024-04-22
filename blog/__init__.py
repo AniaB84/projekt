17dcdce083a467f88c6a512dd18d78cc67be9f4f
AniaB84 <145894449+AniaB84@users.noreply.github.com>
2024-04-22 13:43:57 +0200

    blog/templates/

--- blog/__init__.py
+++ blog/__init__.py
@@ -9,6 +9,8 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 from blog import routes, models
+from faker import Faker
+from blog.models import Entry, db
 
 @app.shell_context_processor
 def make_shell_context():
@@ -18,3 +20,13 @@ def make_shell_context():
  }
 
 
+def generate_entries(how_many=15):
+   fake = Faker()
+   for i in range(how_many):
+       post = Entry(
+       title=fake.sentence(),
+       body='\n'.join(fake.paragraphs(15)),
+       is_published=True
+       )
+       db.session.add(post)
+   db.session.commit()
--- blog/routes.py
+++ blog/routes.py
@@ -1,15 +1,33 @@
-from flask import render_template
-from blog import app, db
-from blog.models import Entry
+from flask import render_template, request
+from blog import app
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
 @app.route("/", endpoint="index")
 def index():
- return render_template("base.html") 
+ return render_template("base.html")
 
 @app.route("/home", endpoint="home")
 def home():
  all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc()).all()
  return render_template("homepage.html", all_posts=all_posts)
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry(
+               title=form.title.data,
+               body=form.body.data,
+               is_published=form.is_published.data
+            )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
 
 if __name__ == "__main__":
     app.run(debug=True)
